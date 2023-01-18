80cbc5cfefc3130fb8763b06bd5526147655df57
itamarc101 <45041126+itamarc101@users.noreply.github.com>
2023-01-18 23:45:26 +0200

    Update snoofer.c

--- snoofer.c
+++ snoofer.c
@@ -56,6 +56,7 @@ void packet_spoof(struct ipheader *ip_packet)
     // information about dest
     dest.sin_family = AF_INET;
     dest.sin_addr = ip_packet->dest_ip;
+    dest.sin_port=0;
 
     // sends packet
     int send = sendto(sock, ip_packet, ntohs(ip_packet->ip_len), 0, (struct sockaddr *)&dest, sizeof(dest));
@@ -66,31 +67,66 @@ void packet_spoof(struct ipheader *ip_packet)
         ip_packet->ip_ihl = 5;
         return;
     }
+
     printf("SUCCESSFULLY SENT A PACKET\n");
     close(sock);
+}
+
+unsigned short calculate_checksum(unsigned short *paddress, int len)
+{
+    int nleft = len;
+    int sum = 0;
+    unsigned short *w = paddress;
+    unsigned short answer = 0;
+
+    while (nleft > 1)
+    {
+        sum += *w++;
+        nleft -= 2;
+    }
+
+    if (nleft == 1)
+    {
+        *((unsigned char *)&answer) = *((unsigned char *)w);
+        sum += answer;
+    }
+
+    // add back carry outs from top 16 bits to low 16 bits
+    sum = (sum >> 16) + (sum & 0xffff); // add hi 16 to low 16
+    sum += (sum >> 16);                 // add carry
+    answer = ~sum;                      // truncate to 16 bits
+
+    return answer;
 }
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
     struct ethheader *ether_packet = (struct ethheader *)packet;
     struct ipheader *ip_packet;
-     ip_packet = (struct ipheader *)(packet + sizeof(struct ethheader));
-    // struct icmpheader *icmp_packet = (struct icmpheader *)((ip_packet->ip_ihl * 4));
+    ip_packet = (struct ipheader *)(packet + sizeof(struct ethheader));
+    struct icmpheader *icmp_packet = (struct icmpheader *)(packet + sizeof(struct ethheader)+sizeof(struct ipheader));
 
+    if(icmp_packet->type==8){   // if type is request
+       
     printf("SEND IT\n");
     char buf[1500];
     memset((char *)buf, 0, 1500);
     memcpy((char *)buf, ip_packet, ntohs(ip_packet->ip_len));
 
     struct ipheader *ip_spoof = (struct ipheader *)buf;
-    struct icmpheader *icmp_spoof = (struct icmpheader *)(buf + (ip_packet->ip_ihl * 4));
+    struct icmpheader *icmp_spoof = (struct icmpheader *)(buf + sizeof(struct ipheader));
+     icmp_spoof->type=0;        // i set packet to be reply
     
+
     ip_spoof->source_ip = ip_packet->dest_ip;
     ip_spoof->dest_ip = ip_packet->source_ip;
-    ip_spoof->ip_ttl = 64;
-    icmp_spoof->type = 0;
+    ip_spoof->ip_checksum = 0;
+    icmp_spoof->checksum = 0;
+    ip_spoof->ip_checksum = (calculate_checksum((unsigned short*)ip_spoof, sizeof(struct ipheader)));
+    icmp_spoof->checksum = (calculate_checksum((unsigned short*)icmp_spoof, sizeof(struct icmpheader)));
 
     packet_spoof(ip_spoof);
+    }
 }
 int main(int argc, char *argv[])
 {
